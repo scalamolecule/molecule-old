a0d9650c4b7de75cb54002c819b3c287913712f2
marcgrue <marcgrue@gmail.com>
2014-11-08 03:02:35 +0100

    Query variables from pattern matching

--- core/src/main/scala/molecule/transform/Model2Query.scala
+++ core/src/main/scala/molecule/transform/Model2Query.scala
@@ -112,72 +112,30 @@ object Model2Query {
     }
 
     val query0 = new Query(Find(Seq()), With(Seq()), In(Seq()), Where(List()))
-
-    //    model.elements.foldLeft((query0, "a", "b")) { case ((query, a, b), element) =>
-    //
-    //      val ns = curNs(element)
-    //
-    //      val (prevNs, prevAttr, prevRefNs) = if (query.wh.clauses.isEmpty)
-    //        ("NoValueYet", "NoValueYet", "")
-    //      else
-    //      // Take info from last clause
-    //        query.wh.clauses.reverse.collectFirst {
-    //          case DataClause(_, _, KW(kwNS, attr, refNs), _, _)      => (kwNS, attr, refNs)
-    //          case Funct("fulltext", Seq(_, KW(kwNS, attr, _), _), _) => (kwNS, attr, "")
-    //        }.getOrElse(sys.error("[Model2Query] Missing NS and Attribute\nmodel: " + model + "\nquery: " + query.toString))
-    //
-    //      val c = (b.toCharArray.head + 1).toChar.toString
-    //      val (entity, value) = {
-    //        if (prevNs == ns) {
-    //          // Previous ns `community` == current ns `community`
-    //          // ?a :community/name ?b                                        a   b
-    //          // ?a :community/url ?c                                         a   c
-    //          (a, c)
-    //        } else if ((prevNs, prevAttr) ==("db", "ident")) {
-    //          // Previous attr is an enumerated value
-    //          // ?a :db/ident ?b                                              a   b
-    //          // ?a :ns/attr ?c                                               a   c
-    //          (a, c)
-    //        } else if (prevAttr == ns) {
-    //          // Previous attr `neighborhood` == current ns `neighborhood`
-    //          // ?a :community/neighborhood ?b                                a   b
-    //          // ?b :neighborhood/name ?c                                     b   c
-    //          (b, c)
-    //        } else if (prevRefNs == ns) {
-    //          // Previous attr `lineItems` is a ref to ns `lineItem` == current ns `lineItem`
-    //          // ?a :order/lineItems ?b                                       a   b
-    //          // ?b :lineItem/product ?c                                      b   c
-    //          (b, c)
-    //        } else {
-    //          // First clause
-    //          // ?a :ns/attr ?b                                               a   b
-    //          (a, b)
-    //        }
-    //      }
-    //      // Add new clause(s) to query
-    //      val updatedQuery = resolve(query, entity, value, element)
-    //      // Take next
-    //      (updatedQuery, entity, value)
-    //    }._1 // Get Query
-
     model.elements.foldLeft((query0, "a", "b", "", "", "")) { case ((query, a, b, prevNs, prevAttr, prevRefNs), element) =>
       val c = (b.toCharArray.head + 1).toChar.toString
-      val (entity, value, curNs, curAttr, curRefNs) = element match {
-        case Atom(ns, attr, _, _, _, _) if ns == prevNs    => x(1, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $a :$ns/$attr $c"); (a, c, ns, attr, "")
-        case Atom(ns, attr, _, _, _, _) if ns == prevAttr  => x(2, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $b :$ns/$attr $c"); (b, c, ns, attr, "")
-        case Atom(ns, attr, _, _, _, _) if ns == prevRefNs => x(3, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $b :$ns/$attr $c"); (b, c, ns, attr, "")
-        case Bond(ns, refAttr, refNs) if ns == prevNs      => x(4, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $a :$ns/$refAttr($refNs) $c"); (a, c, ns, refAttr, refNs)
-        case Bond(ns, refAttr, refNs) if ns == prevAttr    => x(4, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $b :$ns/$refAttr($refNs) $c"); (b, c, ns, refAttr, refNs)
-        case Bond(ns, refAttr, refNs) if ns == prevRefNs   => x(4, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $b :$ns/$refAttr($refNs) $c"); (b, c, ns, refAttr, refNs)
-        case Bond(ns, refAttr, refNs)                      => x(5, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $a :$ns/$refAttr($refNs) $b"); (a, b, ns, refAttr, refNs)
-        case Atom(ns, attr, _, _, _, _)                    => x(6, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $a :$ns/$attr $b"); (a, b, ns, attr, "")
-        case z                                             => x(33, z); (a, b, prevNs, prevAttr, prevRefNs)
-
+      val (a1, b1, ns1, attr1, refNs1) = element match {
+        case Atom(ns, attr, _, _, _, _) if ns == prevNs    => (a, c, ns, attr, "")
+        case Atom(ns, attr, _, _, _, _) if ns == prevAttr  => (b, c, ns, attr, "")
+        case Atom(ns, attr, _, _, _, _) if ns == prevRefNs => (b, c, ns, attr, "")
+        case Atom(ns, attr, _, _, _, _)                    => (a, b, ns, attr, "")
+        case Bond(ns, refAttr, refNs) if ns == prevNs      => (a, c, ns, refAttr, refNs)
+        case Bond(ns, refAttr, refNs) if ns == prevAttr    => (b, c, ns, refAttr, refNs)
+        case Bond(ns, refAttr, refNs) if ns == prevRefNs   => (b, c, ns, refAttr, refNs)
+        case Bond(ns, refAttr, refNs)                      => (a, b, ns, refAttr, refNs)
+        case other                                         => (a, b, prevNs, prevAttr, prevRefNs)
+        //        case Atom(ns, attr, _, _, _, _) if ns == prevNs    => x(1, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $a :$ns/$attr $c"); (a, c, ns, attr, "")
+        //        case Atom(ns, attr, _, _, _, _) if ns == prevAttr  => x(2, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $b :$ns/$attr $c"); (b, c, ns, attr, "")
+        //        case Atom(ns, attr, _, _, _, _) if ns == prevRefNs => x(3, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $b :$ns/$attr $c"); (b, c, ns, attr, "")
+        //        case Atom(ns, attr, _, _, _, _)                    => x(6, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Atom] $a :$ns/$attr $b"); (a, b, ns, attr, "")
+        //        case Bond(ns, refAttr, refNs) if ns == prevNs      => x(4, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $a :$ns/$refAttr($refNs) $c"); (a, c, ns, refAttr, refNs)
+        //        case Bond(ns, refAttr, refNs) if ns == prevAttr    => x(4, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $b :$ns/$refAttr($refNs) $c"); (b, c, ns, refAttr, refNs)
+        //        case Bond(ns, refAttr, refNs) if ns == prevRefNs   => x(4, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $b :$ns/$refAttr($refNs) $c"); (b, c, ns, refAttr, refNs)
+        //        case Bond(ns, refAttr, refNs)                      => x(5, s"[Prev] $a :$prevNs/$prevAttr($prevRefNs) $b", s"[Bond] $a :$ns/$refAttr($refNs) $b"); (a, b, ns, refAttr, refNs)
+        //        case z                                             => x(33, z); (a, b, prevNs, prevAttr, prevRefNs)
       }
-      // Add new clause(s) to query
-      val updatedQuery = resolve(query, entity, value, element)
-      // Take next
-      (updatedQuery, entity, value, curNs, curAttr, curRefNs)
+      val query1 = resolve(query, a1, b1, element)
+      (query1, a1, b1, ns1, attr1, refNs1)
     }._1 // Get Query
   }
 }
