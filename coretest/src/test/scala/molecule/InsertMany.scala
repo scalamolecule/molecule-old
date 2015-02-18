15b64c3564b6e38b6196b4db6e6f504e5ae55a3f
marcgrue <marcgrue@gmail.com>
2015-02-19 00:44:06 +0100

    InsertMany - 1 attribute

--- coretest/src/test/scala/molecule/InsertMany.scala
+++ coretest/src/test/scala/molecule/InsertMany.scala
@@ -10,224 +10,276 @@ import shapeless._
 class InsertMany extends CoreSpec {
 
 
-  "1 attribute" in new CoreSetup {
+  //  "1 attribute" in new CoreSetup {
+  //
+  //    // The `insert` method performs the compile time analysis of the molecule
+  //    // The `apply` method inserts the type-inferred data at runtime
+  //    Ns.strs.insert.apply(Set("a"))
+  //
+  //    // We can enter data for one attribute in 4 different ways:
+  //
+  //    // 1. Set of values
+  //    Ns.strs insert Set("b")
+  //    Ns.strs insert Set("c", "d")
+  //
+  //    // 2. Comma-separated list of sets of values
+  //    Ns.strs.insert(Set("e"), Set("f"))
+  //
+  //    // 3. List of sets of values
+  //    Ns.strs insert List(Set("g"))
+  //    Ns.strs insert List(Set("h", "i"))
+  //    Ns.strs insert List(Set("j"), Set("k"))
+  //
+  //    // 4. Arity-1 HList
+  //    Ns.strs insert Set("l") :: HNil
+  //    Ns.strs insert Set("m", "n") :: HNil
+  //
+  //    // 5. List of Arity-1 HLists
+  //    Ns.strs insert List(Set("o") :: HNil)
+  //    Ns.strs insert List(Set("p", "q") :: HNil)
+  //    Ns.strs insert List(Set("r") :: HNil, Set("s") :: HNil)
+  //
+  //    // All values inserted
+  //    Ns.strs.one.toSeq.sorted === List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s")
+  //
+  //
+  //    Ns.ints.insert(Set(1))
+  //    Ns.ints.insert(Set(2, 3))
+  //    Ns.ints.insert(Set(4), Set(5))
+  //    Ns.ints.insert(List(Set(6)))
+  //    Ns.ints.insert(List(Set(7, 8)))
+  //    Ns.ints.insert(List(Set(9), Set(10)))
+  //
+  //    Ns.ints.insert(Set(11) :: HNil)
+  //    Ns.ints.insert(Set(12, 13) :: HNil)
+  //    Ns.ints.insert(List(Set(14) :: HNil))
+  //    Ns.ints.insert(List(Set(15, 16) :: HNil))
+  //    Ns.ints.insert(List(Set(17) :: HNil, Set(18) :: HNil))
+  //
+  //    Ns.ints.one.toSeq.sorted === List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)
+  //
+  //
+  //    Ns.longs.insert(Set(1L))
+  //    Ns.longs.insert(Set(2L, 3L))
+  //    Ns.longs.insert(Set(4L), Set(5L))
+  //    Ns.longs.insert(List(Set(6L)))
+  //    Ns.longs.insert(List(Set(7L, 8L)))
+  //    Ns.longs.insert(List(Set(9L), Set(10L)))
+  //
+  //    Ns.longs.insert(Set(11L) :: HNil)
+  //    Ns.longs.insert(Set(12L, 13L) :: HNil)
+  //    Ns.longs.insert(List(Set(14L) :: HNil))
+  //    Ns.longs.insert(List(Set(15L, 16L) :: HNil))
+  //    Ns.longs.insert(List(Set(17L) :: HNil, Set(18L) :: HNil))
+  //
+  //    Ns.longs.one.toSeq.sorted === List(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L)
+  //
+  //
+  //    Ns.floats.insert(Set(1.0f))
+  //    Ns.floats.insert(Set(2.0f, 3.0f))
+  //    Ns.floats.insert(Set(4.0f), Set(5.0f))
+  //    Ns.floats.insert(List(Set(6.0f)))
+  //    Ns.floats.insert(List(Set(7.0f, 8.0f)))
+  //    Ns.floats.insert(List(Set(9.0f), Set(10.0f)))
+  //
+  //    Ns.floats.insert(Set(11.0f) :: HNil)
+  //    Ns.floats.insert(Set(12.0f, 13.0f) :: HNil)
+  //    Ns.floats.insert(List(Set(14.0f) :: HNil))
+  //    Ns.floats.insert(List(Set(15.0f, 16.0f) :: HNil))
+  //    Ns.floats.insert(List(Set(17.0f) :: HNil, Set(18.0f) :: HNil))
+  //
+  //    Ns.floats.one.toSeq.sorted === List(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f, 17.0f, 18.0f)
+  //
+  //
+  //    Ns.doubles.insert(Set(1.0))
+  //    Ns.doubles.insert(Set(2.0, 3.0))
+  //    Ns.doubles.insert(Set(4.0), Set(5.0))
+  //    Ns.doubles.insert(List(Set(6.0)))
+  //    Ns.doubles.insert(List(Set(7.0, 8.0)))
+  //    Ns.doubles.insert(List(Set(9.0), Set(10.0)))
+  //
+  //    Ns.doubles.insert(Set(11.0) :: HNil)
+  //    Ns.doubles.insert(Set(12.0, 13.0) :: HNil)
+  //    Ns.doubles.insert(List(Set(14.0) :: HNil))
+  //    Ns.doubles.insert(List(Set(15.0, 16.0) :: HNil))
+  //    Ns.doubles.insert(List(Set(17.0) :: HNil, Set(18.0) :: HNil))
+  //
+  //    Ns.doubles.one.toSeq.sorted === List(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0)
+  //
+  //
+  //    Ns.dates.insert(Set(date1))
+  //    Ns.dates.insert(Set(date2, date3))
+  //    Ns.dates.insert(Set(date4), Set(date5))
+  //    Ns.dates.insert(List(Set(date6)))
+  //    Ns.dates.insert(List(Set(date7, date8)))
+  //    Ns.dates.insert(List(Set(date9), Set(date10)))
+  //
+  //    Ns.dates.insert(Set(date11) :: HNil)
+  //    Ns.dates.insert(Set(date12, date13) :: HNil)
+  //    Ns.dates.insert(List(Set(date14) :: HNil))
+  //    Ns.dates.insert(List(Set(date15, date16) :: HNil))
+  //    Ns.dates.insert(List(Set(date17) :: HNil, Set(date18) :: HNil))
+  //
+  //    Ns.dates.one.toSeq.sorted === List(date1, date2, date3, date4, date5, date6, date7, date8, date9, date10, date11, date12, date13, date14, date15, date16, date17, date18)
+  //
+  //
+  //    Ns.uuids.insert(Set(uuid1))
+  //    Ns.uuids.insert(Set(uuid2, uuid3))
+  //    Ns.uuids.insert(Set(uuid4), Set(uuid5))
+  //    Ns.uuids.insert(List(Set(uuid6)))
+  //    Ns.uuids.insert(List(Set(uuid7, uuid8)))
+  //    Ns.uuids.insert(List(Set(uuid9), Set(uuid10)))
+  //
+  //    Ns.uuids.insert(Set(uuid11) :: HNil)
+  //    Ns.uuids.insert(Set(uuid12, uuid13) :: HNil)
+  //    Ns.uuids.insert(List(Set(uuid14) :: HNil))
+  //    Ns.uuids.insert(List(Set(uuid15, uuid16) :: HNil))
+  //    Ns.uuids.insert(List(Set(uuid17) :: HNil, Set(uuid18) :: HNil))
+  //
+  //    Ns.uuids.one.toSeq.sortBy(_.toString) === List(uuid1, uuid2, uuid3, uuid4, uuid5, uuid6, uuid7, uuid8, uuid9, uuid10, uuid11, uuid12, uuid13, uuid14, uuid15, uuid16, uuid17, uuid18)
+  //
+  //
+  //    Ns.uris.insert(Set(uri1))
+  //    Ns.uris.insert(Set(uri2, uri3))
+  //    Ns.uris.insert(Set(uri4), Set(uri5))
+  //    Ns.uris.insert(List(Set(uri6)))
+  //    Ns.uris.insert(List(Set(uri7, uri8)))
+  //    Ns.uris.insert(List(Set(uri9), Set(uri10)))
+  //
+  //    Ns.uris.insert(Set(uri11) :: HNil)
+  //    Ns.uris.insert(Set(uri12, uri13) :: HNil)
+  //    Ns.uris.insert(List(Set(uri14) :: HNil))
+  //    Ns.uris.insert(List(Set(uri15, uri16) :: HNil))
+  //    Ns.uris.insert(List(Set(uri17) :: HNil, Set(uri18) :: HNil))
+  //
+  //    Ns.uris.one.toSeq.sortBy(_.toString) === List(uri1, uri2, uri3, uri4, uri5, uri6, uri7, uri8, uri9, uri10, uri11, uri12, uri13, uri14, uri15, uri16, uri17, uri18)
+  //
+  //
+  //    Ns.enums.insert(Set(enum1))
+  //    Ns.enums.insert(Set(enum2, enum3))
+  //    Ns.enums.insert(Set(enum4), Set(enum5))
+  //    Ns.enums.insert(List(Set(enum6)))
+  //    Ns.enums.insert(List(Set(enum7, enum8)))
+  //    Ns.enums.insert(List(Set(enum9), Set(enum0)))
+  //
+  //    Ns.enums.insert(Set(enum1) :: HNil)
+  //    Ns.enums.insert(Set(enum2, enum3) :: HNil)
+  //    Ns.enums.insert(List(Set(enum4) :: HNil))
+  //    Ns.enums.insert(List(Set(enum5, enum6) :: HNil))
+  //    Ns.enums.insert(List(Set(enum7) :: HNil, Set(enum8) :: HNil))
+  //
+  //    Ns.enums.one.toSeq.sorted === List(enum0, enum1, enum2, enum3, enum4, enum5, enum6, enum7, enum8, enum9)
+  //  }
 
-    // The `insert` method performs the compile time analysis of the molecule
-    // The `apply` method inserts the type-inferred data at runtime
-    Ns.strs.insert.apply(Set("a"))
 
-    // We can enter data for one attribute in 4 different ways:
+  "Data-molecule, 1 attr" in new CoreSetup {
 
-    // 1. Set of values
-    Ns.strs insert Set("b")
-    Ns.strs insert Set("c", "d")
+    // Construct a "Data-Molecule" with an attribute value and add it to the database
 
-    // 2. Comma-separated list of sets of values
-    Ns.strs.insert(Set("e"), Set("f"))
+    Ns.strs("a").add
+    Ns.strs("b", "c").add
+    Ns.strs.one === Set("a", "b", "c")
 
-    // 3. List of sets of values
-    Ns.strs insert List(Set("g"))
-    Ns.strs insert List(Set("h", "i"))
-    Ns.strs insert List(Set("j"), Set("k"))
+    Ns.ints(1).add
+    Ns.ints(2, 3).add
+    Ns.ints.one === Set(1, 2, 3)
 
-    // 4. Arity-1 HList
-    Ns.strs insert Set("l") :: HNil
-    Ns.strs insert Set("m", "n") :: HNil
+    Ns.longs(1L).add
+    Ns.longs(2L, 3L).add
+    Ns.longs.one === Set(1L, 2L, 3L)
 
-    // 5. List of Arity-1 HLists
-    Ns.strs insert List(Set("o") :: HNil)
-    Ns.strs insert List(Set("p", "q") :: HNil)
-    Ns.strs insert List(Set("r") :: HNil, Set("s") :: HNil)
+    Ns.floats(1.0f).add
+    Ns.floats(2.0f, 3.0f).add
+    Ns.floats.one === Set(1.0f, 2.0f, 3.0f)
 
-    // All values inserted
-    Ns.strs.one.toSeq.sorted === List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s")
+    Ns.doubles(1.0).add
+    Ns.doubles(2.0, 3.0).add
+    Ns.doubles.one === Set(1.0, 2.0, 3.0)
 
+    // Ns.bools not implemented...
 
-    Ns.ints.insert(Set(1))
-    Ns.ints.insert(Set(2, 3))
-    Ns.ints.insert(Set(4), Set(5))
-    Ns.ints.insert(List(Set(6)))
-    Ns.ints.insert(List(Set(7, 8)))
-    Ns.ints.insert(List(Set(9), Set(10)))
+    Ns.dates(date1).add
+    Ns.dates(date2, date3).add
+    Ns.dates.one === Set(date1, date2, date3)
 
-    Ns.ints.insert(Set(11) :: HNil)
-    Ns.ints.insert(Set(12, 13) :: HNil)
-    Ns.ints.insert(List(Set(14) :: HNil))
-    Ns.ints.insert(List(Set(15, 16) :: HNil))
-    Ns.ints.insert(List(Set(17) :: HNil, Set(18) :: HNil))
+    Ns.uuids(uuid1).add
+    Ns.uuids(uuid2, uuid3).add
+    Ns.uuids.one === Set(uuid1, uuid2, uuid3)
 
-    Ns.ints.one.toSeq.sorted === List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)
+    Ns.uris(uri1).add
+    Ns.uris(uri2, uri3).add
+    Ns.uris.one === Set(uri1, uri2, uri3)
 
-
-    Ns.longs.insert(Set(1L))
-    Ns.longs.insert(Set(2L, 3L))
-    Ns.longs.insert(Set(4L), Set(5L))
-    Ns.longs.insert(List(Set(6L)))
-    Ns.longs.insert(List(Set(7L, 8L)))
-    Ns.longs.insert(List(Set(9L), Set(10L)))
-
-    Ns.longs.insert(Set(11L) :: HNil)
-    Ns.longs.insert(Set(12L, 13L) :: HNil)
-    Ns.longs.insert(List(Set(14L) :: HNil))
-    Ns.longs.insert(List(Set(15L, 16L) :: HNil))
-    Ns.longs.insert(List(Set(17L) :: HNil, Set(18L) :: HNil))
-
-    Ns.longs.one.toSeq.sorted === List(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L)
-
-
-    Ns.floats.insert(Set(1.0f))
-    Ns.floats.insert(Set(2.0f, 3.0f))
-    Ns.floats.insert(Set(4.0f), Set(5.0f))
-    Ns.floats.insert(List(Set(6.0f)))
-    Ns.floats.insert(List(Set(7.0f, 8.0f)))
-    Ns.floats.insert(List(Set(9.0f), Set(10.0f)))
-
-    Ns.floats.insert(Set(11.0f) :: HNil)
-    Ns.floats.insert(Set(12.0f, 13.0f) :: HNil)
-    Ns.floats.insert(List(Set(14.0f) :: HNil))
-    Ns.floats.insert(List(Set(15.0f, 16.0f) :: HNil))
-    Ns.floats.insert(List(Set(17.0f) :: HNil, Set(18.0f) :: HNil))
-
-    Ns.floats.one.toSeq.sorted === List(1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f, 17.0f, 18.0f)
-
-
-    Ns.doubles.insert(Set(1.0))
-    Ns.doubles.insert(Set(2.0, 3.0))
-    Ns.doubles.insert(Set(4.0), Set(5.0))
-    Ns.doubles.insert(List(Set(6.0)))
-    Ns.doubles.insert(List(Set(7.0, 8.0)))
-    Ns.doubles.insert(List(Set(9.0), Set(10.0)))
-
-    Ns.doubles.insert(Set(11.0) :: HNil)
-    Ns.doubles.insert(Set(12.0, 13.0) :: HNil)
-    Ns.doubles.insert(List(Set(14.0) :: HNil))
-    Ns.doubles.insert(List(Set(15.0, 16.0) :: HNil))
-    Ns.doubles.insert(List(Set(17.0) :: HNil, Set(18.0) :: HNil))
-
-    Ns.doubles.one.toSeq.sorted === List(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0)
-
-
-    Ns.dates.insert(Set(date1))
-    Ns.dates.insert(Set(date2, date3))
-    Ns.dates.insert(Set(date4), Set(date5))
-    Ns.dates.insert(List(Set(date6)))
-    Ns.dates.insert(List(Set(date7, date8)))
-    Ns.dates.insert(List(Set(date9), Set(date10)))
-
-    Ns.dates.insert(Set(date11) :: HNil)
-    Ns.dates.insert(Set(date12, date13) :: HNil)
-    Ns.dates.insert(List(Set(date14) :: HNil))
-    Ns.dates.insert(List(Set(date15, date16) :: HNil))
-    Ns.dates.insert(List(Set(date17) :: HNil, Set(date18) :: HNil))
-
-    Ns.dates.one.toSeq.sorted === List(date1, date2, date3, date4, date5, date6, date7, date8, date9, date10, date11, date12, date13, date14, date15, date16, date17, date18)
-
-
-    Ns.uuids.insert(Set(uuid1))
-    Ns.uuids.insert(Set(uuid2, uuid3))
-    Ns.uuids.insert(Set(uuid4), Set(uuid5))
-    Ns.uuids.insert(List(Set(uuid6)))
-    Ns.uuids.insert(List(Set(uuid7, uuid8)))
-    Ns.uuids.insert(List(Set(uuid9), Set(uuid10)))
-
-    Ns.uuids.insert(Set(uuid11) :: HNil)
-    Ns.uuids.insert(Set(uuid12, uuid13) :: HNil)
-    Ns.uuids.insert(List(Set(uuid14) :: HNil))
-    Ns.uuids.insert(List(Set(uuid15, uuid16) :: HNil))
-    Ns.uuids.insert(List(Set(uuid17) :: HNil, Set(uuid18) :: HNil))
-
-    Ns.uuids.one.toSeq.sortBy(_.toString) === List(uuid1, uuid2, uuid3, uuid4, uuid5, uuid6, uuid7, uuid8, uuid9, uuid10, uuid11, uuid12, uuid13, uuid14, uuid15, uuid16, uuid17, uuid18)
-
-
-    Ns.uris.insert(Set(uri1))
-    Ns.uris.insert(Set(uri2, uri3))
-    Ns.uris.insert(Set(uri4), Set(uri5))
-    Ns.uris.insert(List(Set(uri6)))
-    Ns.uris.insert(List(Set(uri7, uri8)))
-    Ns.uris.insert(List(Set(uri9), Set(uri10)))
-
-    Ns.uris.insert(Set(uri11) :: HNil)
-    Ns.uris.insert(Set(uri12, uri13) :: HNil)
-    Ns.uris.insert(List(Set(uri14) :: HNil))
-    Ns.uris.insert(List(Set(uri15, uri16) :: HNil))
-    Ns.uris.insert(List(Set(uri17) :: HNil, Set(uri18) :: HNil))
-
-    Ns.uris.one.toSeq.sortBy(_.toString) === List(uri1, uri2, uri3, uri4, uri5, uri6, uri7, uri8, uri9, uri10, uri11, uri12, uri13, uri14, uri15, uri16, uri17, uri18)
-
-
-    Ns.enums.insert(Set(enum1))
-    Ns.enums.insert(Set(enum2, enum3))
-    Ns.enums.insert(Set(enum4), Set(enum5))
-    Ns.enums.insert(List(Set(enum6)))
-    Ns.enums.insert(List(Set(enum7, enum8)))
-    Ns.enums.insert(List(Set(enum9), Set(enum0)))
-
-    Ns.enums.insert(Set(enum1) :: HNil)
-    Ns.enums.insert(Set(enum2, enum3) :: HNil)
-    Ns.enums.insert(List(Set(enum4) :: HNil))
-    Ns.enums.insert(List(Set(enum5, enum6) :: HNil))
-    Ns.enums.insert(List(Set(enum7) :: HNil, Set(enum8) :: HNil))
-
-    Ns.enums.one.toSeq.sorted === List(enum0, enum1, enum2, enum3, enum4, enum5, enum6, enum7, enum8, enum9)
+    Ns.enums("enum1").add
+    Ns.enums("enum2", "enum3").add
+    Ns.enums.one === Set("enum1", "enum2", "enum3")
   }
 
 
-  //  "Data-molecule, 1 attr" in new CoreSetup {
-  //
-  //    // Construct a "Data-Molecule" with an attribute value and add it to the database
-  //
-  //    Ns.str("a").add
-  //    Ns.int(1).add
-  //    Ns.long(1L).add
-  //    Ns.float(1.0f).add
-  //    Ns.double(1.0).add
-  //    Ns.bool(true).add
-  //    Ns.date(date1).add
-  //    Ns.uuid(uuid1).add
-  //    Ns.uri(uri1).add
-  //    Ns.enum("enum1").add
-  //
-  //    Ns.str.one === "a"
-  //    Ns.int.one === 1
-  //    Ns.long.one === 1L
-  //    Ns.float.one === 1.0f
-  //    Ns.double.one === 1.0
-  //    Ns.bool.one === true
-  //    Ns.date.one === date1
-  //    Ns.uuid.one === uuid1
-  //    Ns.uri.one === uri1
-  //    Ns.enum.one === enum1
-  //  }
-  //
-  //
-  //  "Data-molecule, n attrs" in new CoreSetup {
-  //
-  //    // Construct a "Data-Molecule" with multiple attributes populated with data and add it to the database
-  //
-  //    Ns.str("a").int(1).long(1L).float(1.0f).double(1.0).bool(true).date(date1).uuid(uuid1).uri(uri1).enum("enum1").add
-  //
-  //    Ns.str.int.long.float.double.bool.date.uuid.uri.enum.one ===(
-  //      "a", 1, 1L, 1.0f, 1.0, true, date1, uuid1, uri1, "enum1")
-  //  }
-  //
-  //
-  //  " Insert-Molecule n attributes" in new CoreSetup {
-  //
-  //    // Insert 3 entities as tuples of values
-  //    // Note that values are typechecked against the attribute types of the molecule
-  //    Ns.str.int.long.float.double.bool.date.uuid.uri.enum insert List(
-  //      (" ", 0, 0L, 0.0f, 0.0, false, date0, uuid0, uri0, "enum0"),
-  //      ("a", 1, 1L, 1.0f, 1.0, true, date1, uuid1, uri1, "enum1"),
-  //      ("b", 2, 2L, 2.0f, 2.0, false, date2, uuid2, uri2, "enum2")
-  //    )
-  //
-  //    Ns.str.int.long.float.double.bool.date.uuid.uri.enum.get.sortBy(_._1) === List(
-  //      (" ", 0, 0L, 0.0f, 0.0, false, date0, uuid0, uri0, "enum0"),
-  //      ("a", 1, 1L, 1.0f, 1.0, true, date1, uuid1, uri1, "enum1"),
-  //      ("b", 2, 2L, 2.0f, 2.0, false, date2, uuid2, uri2, "enum2")
-  //    )
-  //  }
-  //
+  "Data-molecule, n attrs" in new CoreSetup {
+
+    // Construct a "Data-Molecule" with multiple attributes populated with data and add it to the database
+
+    Ns.strs("a", "b")
+      .ints(1, 2)
+      .longs(1L, 2L)
+      .floats(1.0f, 2.0f)
+      .doubles(1.0, 2.0)
+      .dates(date1, date2)
+      .uuids(uuid1, uuid2)
+      .uris(uri1, uri2)
+      .enums("enum1", "enum2").add
+
+    Ns.strs.ints.longs.floats.doubles.dates.uuids.uris.enums.one ===(
+      Set("a", "b"),
+      Set(1, 2),
+      Set(1L, 2L),
+      Set(1.0f, 2.0f),
+      Set(1.0, 2.0),
+      Set(date1, date2),
+      Set(uuid1, uuid2),
+      Set(uri1, uri2),
+      Set("enum1", "enum2"))
+  }
+
+
+  " Insert-Molecule n attributes" in new CoreSetup {
+
+    // Insert 3 entities as tuples of values
+    // Note that values are typechecked against the attribute types of the molecule
+    Ns.strs.ints.longs.floats.doubles.dates.uuids.uris.enums insert List(
+      (Set("a", "b"),
+        Set(1, 2),
+        Set(1L, 2L),
+        Set(1.0f, 2.0f),
+        Set(1.0, 2.0),
+        Set(date1, date2),
+        Set(uuid1, uuid2),
+        Set(uri1, uri2),
+        Set("enum1", "enum2")),
+      (Set("c", "d"),
+        Set(3, 4),
+        Set(3L, 4L),
+        Set(3.0f, 4.0f),
+        Set(3.0, 4.0),
+        Set(date3, date4),
+        Set(uuid3, uuid4),
+        Set(uri3, uri4),
+        Set("enum3", "enum4"))
+    )
+
+    Ns.strs.ints.longs.floats.doubles.dates.uuids.uris.enums.one === (
+      Set("d", "a", "b", "c"),
+      Set(1, 4, 3, 2),
+      Set(1L, 4L, 3L, 2L),
+      Set(2.0f, 4.0f, 1.0f, 3.0f),
+      Set(2.0, 4.0, 1.0, 3.0),
+      Set(date4, date1, date2, date3),
+      Set(uuid3, uuid2, uuid4, uuid1),
+      Set(uri1, uri2, uri3, uri4),
+      Set("enum4", "enum1", "enum3", "enum2"))
+  }
+
   //
   //  " Insert-Molecule (2-step insertion)" in new CoreSetup {
   //
--- coretest/src/test/scala/molecule/util/CoreSpec.scala
+++ coretest/src/test/scala/molecule/util/CoreSpec.scala
@@ -10,16 +10,15 @@ import org.specs2.specification.Scope
 
 class CoreSetup extends Scope with DatomicFacade {
   implicit val conn = load(CoreTestSchema.tx)
-  //  implicit val conn = load(CoreTestSchema.tx, "core")
 }
 
 class CoreSpec extends MoleculeSpec with DatomicFacade {
 
-  // Sample data
+  def da(s: Int) = new Date(1000L * s)
   def uu = randomUUID()
   def ur(i: Int) = new URI("uri" + i)
-  def da(s: Int) = new Date(1000L * s)
 
+  // Sample data
 
   lazy val (date0, date1, date2, date3, date4, date5, date6, date7, date8, date9, date10, date11, date12, date13, date14, date15, date16, date17, date18)     =
     (da(0), da(1), da(2), da(3), da(4), da(5), da(6), da(7), da(8), da(9), da(10), da(11), da(12), da(13), da(14), da(15), da(16), da(17), da(18))
@@ -28,7 +27,7 @@ class CoreSpec extends MoleculeSpec with DatomicFacade {
     List(uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu, uu).sortBy(_.toString)
 
   lazy val List(uri0, uri1, uri2, uri3, uri4, uri5, uri6, uri7, uri8, uri9, uri10, uri11, uri12, uri13, uri14, uri15, uri16, uri17, uri18)                    =
-    List(ur(0), ur(1), ur(2), ur(3), ur(4), ur(5), ur(6), ur(7), ur(8), ur(9), ur(10), ur(11), ur(12), ur(13), ur(14), ur(15), ur(16), ur(17), ur(18)).sortBy(_.toString)
+    List(ur(0), ur(1), ur(2), ur(3), ur(4), ur(5), ur(6), ur(7), ur(8), ur(9), ur(10), ur(11), ur(12), ur(13), ur(14), ur(15), ur(16), ur(17), ur(18)) //.sortBy(_.toString)
 
   lazy val (str0, int0, long0, float0, double0, bool0, enum0) = (" ", 0, 0L, 0.0f, 0.0, false, "enum0")
   lazy val (str1, int1, long1, float1, double1, bool1, enum1) = ("a", 1, 1L, 1.0f, 1.0, true, "enum1")
@@ -41,16 +40,6 @@ class CoreSpec extends MoleculeSpec with DatomicFacade {
   lazy val (str8, int8, long8, float8, double8, bool8, enum8) = ("h", 4, 4L, 4.0f, 4.0, false, "enum8")
   lazy val (str9, int9, long9, float9, double9, bool9, enum9) = ("i", 4, 4L, 4.0f, 4.0, true, "enum9")
 
-  //  lazy val (strs0, ints0, longs0, floats0, doubles0, dates0, uuids0, uris0, enums0) = (
-  //    Set[String](),
-  //    Set[Int](),
-  //    Set[Long](),
-  //    Set[Float](),
-  //    Set[Double](),
-  //    Set[Date](),
-  //    Set[UUID](),
-  //    Set[URI](),
-  //    Set[String]())
 
   lazy val (strs0, ints0, longs0, floats0, doubles0, dates0, uuids0, uris0, enums0) = (
     Set(str0),
@@ -84,15 +73,4 @@ class CoreSpec extends MoleculeSpec with DatomicFacade {
     Set(uuid1, uuid2),
     Set(uri1, uri2),
     Set(enum1, enum2))
-
-  //  lazy val (strs3, ints3, longs3, floats3, doubles3, dates3, uuids3, uris3, enums3) = (
-  //    Set(str0, str1, str2),
-  //    Set(int0, int1, int2),
-  //    Set(long0, long1, long2),
-  //    Set(float0, float1, float2),
-  //    Set(double0, double1, double2),
-  //    Set(date0, date1, date2),
-  //    Set(uuid0, uuid1, uuid2),
-  //    Set(uri0, uri1, uri2),
-  //    Set(enum0, enum1, enum2))
 }
