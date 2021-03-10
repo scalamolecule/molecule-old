efa5f41ea597bb943b904a73ba6072f5eff95364
marcgrue <marcgrue@gmail.com>
2021-03-10 10:31:33 +0100

    No overloading with default arguments

--- molecule-core/jvm/src/main/scala-2.13+/molecule/core/api/get/GetList.scala
+++ molecule-core/jvm/src/main/scala-2.13+/molecule/core/api/get/GetList.scala
@@ -109,7 +109,62 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
 
   // get as of ================================================================================================
 
-  /** Get `List` of rows as tuples matching molecule as of transaction time `t`.
+  /** Get `List` of all rows as tuples matching molecule as of transaction time `t`.
+    * <br><br>
+    * Transaction time `t` is an auto-incremented transaction number assigned internally by Datomic.
+    * <br><br>
+    * `t` can for instance be retrieved in a getHistory call for an attribute and then be
+    * used to get data as of that point in time (including that transaction):
+    * {{{
+    *   // Insert (t 1028)
+    *   val List(ben, liz) = Person.name.age insert List(
+    *     ("Ben", 42),
+    *     ("Liz", 37),
+    *   ) eids
+    *
+    *   // Update (t 1031)
+    *   Person(ben).age(43).update
+    *
+    *   // Retract (t 1032)
+    *   ben.retract
+    *
+    *   // History of Ben
+    *   Person(ben).age.t.op.getHistory.sortBy(r => (r._2, r._3)) === List(
+    *     (42, 1028, true),  // Insert:  42 asserted
+    *     (42, 1031, false), // Update:  42 retracted
+    *     (43, 1031, true),  //          43 asserted
+    *     (43, 1032, false)  // Retract: 43 retracted
+    *   )
+    *
+    *   // Get List of all rows as of transaction t 1028 (after insert)
+    *   Person.name.age.getAsOf(1028) === List(
+    *     ("Liz", 37),
+    *     ("Ben", 42)
+    *   )
+    *
+    *   // Get List of all rows as of transaction t 1031 (after update)
+    *   Person.name.age.getAsOf(1031) === List(
+    *     ("Liz", 37),
+    *     ("Ben", 43)
+    *   )
+    *
+    *   // Get List of all rows as of transaction t 1032 (after retract)
+    *   Person.name.age.getAsOf(1032) === List(
+    *     ("Liz", 37)
+    *   )
+    * }}}
+    *
+    * @group getAsOf
+    * @param t    Transaction time t
+    * @param conn Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncAsOf(t:Long)* getAsyncAsOf]] method.
+    */
+  def getAsOf(t: Long)(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(AsOf(TxLong(t))))
+
+
+  /** Get `List` of n rows as tuples matching molecule as of transaction time `t`.
     * <br><br>
     * Transaction time `t` is an auto-incremented transaction number assigned internally by Datomic.
     * <br><br>
@@ -142,11 +197,6 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     *   Person.name.age.getAsOf(1031, 1) === List(
     *     ("Ben", 43)
     *   )
-    *
-    *   // Get List of all rows as of transaction t 1032 (after retract)
-    *   Person.name.age.getAsOf(1032) === List(
-    *     ("Liz", 37)
-    *   )
     * }}}
     *
     * @group getAsOf
@@ -160,7 +210,58 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     get(n)(conn.usingTempDb(AsOf(TxLong(t))))
 
 
-  /** Get `List` of rows as tuples matching molecule as of tx.
+  /** Get `List` of all rows as tuples matching molecule as of tx.
+    * <br><br>
+    * Datomic's internal `asOf` method can take a transaction entity id as argument to retrieve a
+    * database value as of that transaction (including).
+    * <br><br>
+    * Instead of supplying the transaction entity id, in Molecule we supply a [[molecule.datomic.base.facade.TxReport TxReport]]
+    * that contains the transaction entity id (which is used as argument to Datomic internally). This is more
+    * convenient when using Molecule since we get a [[molecule.datomic.base.facade.TxReport TxReport]] from transaction
+    * operations like `get`, `update`, `retract` etc.
+    * {{{
+    *   // Insert (tx report 1)
+    *   val tx1 = Person.name.age insert List(
+    *     ("Ben", 42),
+    *     ("Liz", 37),
+    *   )
+    *   val List(ben, liz) = tx1.eids
+    *
+    *   // Update (tx report 2)
+    *   val tx2 = Person(ben).age(43).update
+    *
+    *   // Retract (tx report 3)
+    *   val tx3 = ben.retract
+    *
+    *   // Get List of all rows as of tx1 (after insert)
+    *   Person.name.age.getAsOf(tx1) === List(
+    *     ("Ben", 42),
+    *     ("Liz", 37)
+    *   )
+    *
+    *   // Get List of all rows as of tx2 (after update)
+    *   Person.name.age.getAsOf(tx2) === List(
+    *     ("Ben", 43), // Ben now 43
+    *     ("Liz", 37)
+    *   )
+    *
+    *   // Get List of all rows as of tx3 (after retract)
+    *   Person.name.age.getAsOf(tx3) === List(
+    *     ("Liz", 37) // Ben gone
+    *   )
+    * }}}
+    *
+    * @group getAsOf
+    * @param tx   [[molecule.datomic.base.facade.TxReport TxReport]] (returned from all molecule transaction operations)
+    * @param conn Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncAsOf(tx:molecule\.datomic\.base\.facade\.TxReport)* getAsyncAsOf]] method.
+    * */
+  def getAsOf(tx: TxReport)(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(AsOf(TxLong(tx.t))))
+
+
+  /** Get `List` of n rows as tuples matching molecule as of tx.
     * <br><br>
     * Datomic's internal `asOf` method can take a transaction entity id as argument to retrieve a database
     * value as of that transaction (including).
@@ -205,11 +306,64 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     * @return List[Tpl] where Tpl is a tuple of data matching molecule
     * @see Equivalent asynchronous [[GetAsyncList.getAsyncAsOf(tx:molecule\.datomic\.base\.facade\.TxReport,n:Int)* getAsyncAsOf]] method.
     * */
-  def getAsOf(tx: TxReport, n: Int = -1)(implicit conn: Conn): List[Tpl] =
+  def getAsOf(tx: TxReport, n: Int)(implicit conn: Conn): List[Tpl] =
     get(n)(conn.usingTempDb(AsOf(TxLong(tx.t))))
 
 
-  /** Get `List` of rows as tuples matching molecule as of date.
+  /** Get `List` of all rows as tuples matching molecule as of date.
+    * <br><br>
+    * Get data at a human point in time (a java.util.Date).
+    * {{{
+    *   val beforeInsert = new java.util.Date
+    *
+    *   // Insert
+    *   val tx1 = Person.name.age insert List(
+    *     ("Ben", 42),
+    *     ("Liz", 37),
+    *   )
+    *   val List(ben, liz) = tx1.eids
+    *   val afterInsert = new java.util.Date
+    *
+    *   // Update
+    *   val tx2 = Person(ben).age(43).update
+    *   val afterUpdate = new java.util.Date
+    *
+    *   // Retract
+    *   val tx3 = ben.retract
+    *   val afterRetract = new java.util.Date
+    *
+    *   // No data yet before insert
+    *   Person.name.age.getAsOf(beforeInsert) === Nil
+    *
+    *   // Get List of all rows as of afterInsert
+    *   Person.name.age.getAsOf(afterInsert) === List(
+    *     ("Ben", 42),
+    *     ("Liz", 37)´
+    *   )
+    *
+    *   // Get List of all rows as of afterUpdate
+    *   Person.name.age.getAsOf(afterUpdate) === List(
+    *     ("Ben", 43), // Ben now 43
+    *     ("Liz", 37)
+    *   )
+    *
+    *   // Get List of all rows as of afterRetract
+    *   Person.name.age.getAsOf(afterRetract) === List(
+    *     ("Liz", 37) // Ben gone
+    *   )
+    * }}}
+    *
+    * @group getAsOf
+    * @param date java.util.Date
+    * @param conn Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncAsOf(date:java\.util\.Date)* getAsyncAsOf]] method.
+    */
+  def getAsOf(date: Date)(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(AsOf(TxDate(date))))
+
+
+  /** Get `List` of n rows as tuples matching molecule as of date.
     * <br><br>
     * Get data at a human point in time (a java.util.Date).
     * {{{
@@ -237,11 +391,6 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     *   Person.name.age.getAsOf(afterUpdate, 1) === List(
     *     ("Ben", 43)
     *   )
-    *
-    *   // Get List of all rows as of afterRetract
-    *   Person.name.age.getAsOf(afterRetract) === List(
-    *     ("Liz", 37) // Ben gone
-    *   )
     * }}}
     *
     * @group getAsOf
@@ -251,13 +400,48 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     * @return List[Tpl] where Tpl is a tuple of data matching molecule
     * @see Equivalent asynchronous [[GetAsyncList.getAsyncAsOf(date:java\.util\.Date,n:Int)* getAsyncAsOf]] method.
     */
-  def getAsOf(date: Date, n: Int = -1)(implicit conn: Conn): List[Tpl] =
+  def getAsOf(date: Date, n: Int)(implicit conn: Conn): List[Tpl] =
     get(n)(conn.usingTempDb(AsOf(TxDate(date))))
 
 
   // get since ================================================================================================
 
-  /** Get `List` of rows as tuples matching molecule since transaction time `t`.
+  /** Get `List` of all rows as tuples matching molecule since transaction time `t`.
+    * <br><br>
+    * Transaction time `t` is an auto-incremented transaction number assigned internally by Datomic.
+    * <br><br>
+    * `t` can for instance be retrieved by calling `t` on the tx report returned from transactional operations
+    * and then be used to get data since that point in time (excluding that transaction):
+    * {{{
+    *   // 3 transaction times `t`
+    *   val t1 = Person.name("Ann").save.t
+    *   val t2 = Person.name("Ben").save.t
+    *   val t3 = Person.name("Cay").save.t
+    *
+    *   // Current values
+    *   Person.name.get === List("Ann", "Ben", "Cay")
+    *
+    *   // Ben and Cay added since transaction time t 1028
+    *   Person.name.getSince(t1) === List("Ben", "Cay")
+    *
+    *   // Cay added since transaction time t 1030
+    *   Person.name.getSince(t2) === List("Cay")
+    *
+    *   // Nothing added since transaction time t 1032
+    *   Person.name.getSince(t3) === Nil
+    * }}}
+    *
+    * @group getSince
+    * @param t    Transaction time t
+    * @param conn Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncSince(t:Long)* getAsyncSince]] method.
+    */
+  def getSince(t: Long)(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(Since(TxLong(t))))
+
+
+  /** Get `List` of n rows as tuples matching molecule since transaction time `t`.
     * <br><br>
     * Transaction time `t` is an auto-incremented transaction number assigned internally by Datomic.
     * <br><br>
@@ -277,12 +461,6 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     *
     *   // Ben and Cay added since transaction time t 1028 - only n (1) rows returned
     *   Person.name.getSince(t1, 1) === List("Ben")
-    *
-    *   // Cay added since transaction time t 1030
-    *   Person.name.getSince(t2) === List("Cay")
-    *
-    *   // Nothing added since transaction time t 1032
-    *   Person.name.getSince(t3) === Nil
     * }}}
     *
     * @group getSince
@@ -292,11 +470,48 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     * @return List[Tpl] where Tpl is a tuple of data matching molecule
     * @see Equivalent asynchronous [[GetAsyncList.getAsyncSince(t:Long,n:Int)* getAsyncSince]] method.
     */
-  def getSince(t: Long, n: Int = -1)(implicit conn: Conn): List[Tpl] =
+  def getSince(t: Long, n: Int)(implicit conn: Conn): List[Tpl] =
     get(n)(conn.usingTempDb(Since(TxLong(t))))
 
 
-  /** Get `List` of rows as tuples matching molecule since tx.
+  /** Get `List` of all rows as tuples matching molecule since tx.
+    * <br><br>
+    * Datomic's internal `since` can take a transaction entity id as argument to retrieve a database
+    * value since that transaction (excluding the transaction itself).
+    * <br><br>
+    * Instead of supplying the transaction entity id, in Molecule we supply a [[molecule.datomic.base.facade.TxReport TxReport]] that contains
+    * the transaction entity id (which is used as argument to Datomic internally). This is more convenient when using Molecule since we
+    * getAsync a [[molecule.datomic.base.facade.TxReport TxReport]] from transaction operations like `get`, `update`, `retract` etc.
+    * {{{
+    *   // Get tx reports for 3 transactions
+    *   val tx1 = Person.name("Ann").save
+    *   val tx2 = Person.name("Ben").save
+    *   val tx3 = Person.name("Cay").save
+    *
+    *   // Current values
+    *   Person.name.get === List("Ann", "Ben", "Cay")
+    *
+    *   // Ben and Cay added since tx1
+    *   Person.name.getSince(tx1) === List("Ben", "Cay")
+    *
+    *   // Cay added since tx2
+    *   Person.name.getSince(tx2) === List("Cay")
+    *
+    *   // Nothing added since tx3
+    *   Person.name.getSince(tx3) === Nil
+    * }}}
+    *
+    * @group getSince
+    * @param tx   [[molecule.datomic.base.facade.TxReport TxReport]]
+    * @param conn Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncSince(tx:molecule\.datomic\.base\.facade\.TxReport)* getAsyncSince]] method.
+    */
+  def getSince(tx: TxReport)(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(Since(TxLong(tx.t))))
+
+
+  /** Get `List` of n rows as tuples matching molecule since tx.
     * <br><br>
     * Datomic's internal `since` can take a transaction entity id as argument to retrieve a database
     * value since that transaction (excluding the transaction itself).
@@ -318,12 +533,6 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     *
     *   // Ben and Cay added since tx1 - only n (1) rows returned
     *   Person.name.getSince(tx1, 1) === List("Ben")
-    *
-    *   // Cay added since tx2
-    *   Person.name.getSince(tx2) === List("Cay")
-    *
-    *   // Nothing added since tx3
-    *   Person.name.getSince(tx3) === Nil
     * }}}
     *
     * @group getSince
@@ -333,11 +542,43 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     * @return List[Tpl] where Tpl is a tuple of data matching molecule
     * @see Equivalent asynchronous [[GetAsyncList.getAsyncSince(tx:molecule\.datomic\.base\.facade\.TxReport,n:Int)* getAsyncSince]] method.
     */
-  def getSince(tx: TxReport, n: Int = -1)(implicit conn: Conn): List[Tpl] =
+  def getSince(tx: TxReport, n: Int)(implicit conn: Conn): List[Tpl] =
     get(n)(conn.usingTempDb(Since(TxLong(tx.t))))
 
 
-  /** Get `List` of rows as tuples matching molecule since date.
+  /** Get `List` of all rows as tuples matching molecule since date.
+    * <br><br>
+    * Get data added/retracted since a human point in time (a java.util.Date).
+    * {{{
+    *   // Transact 3 times (`inst` retrieves transaction time/Date from tx report)
+    *   val date1 = Person.name("Ann").save.inst
+    *   val date2 = Person.name("Ben").save.inst
+    *   val date3 = Person.name("Cay").save.inst
+    *
+    *   // Current values
+    *   Person.name.get === List("Ann", "Ben", "Cay")
+    *
+    *   // Ben and Cay added since date1
+    *   Person.name.getSince(date1) === List("Ben", "Cay")
+    *
+    *   // Cay added since date2
+    *   Person.name.getSince(date2) === List("Cay")
+    *
+    *   // Nothing added since date3
+    *   Person.name.getSince(date3) === Nil
+    * }}}
+    *
+    * @group getSince
+    * @param date java.util.Date
+    * @param conn Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncSince(date:java\.util\.Date)* getAsyncSince]] method.
+    */
+  def getSince(date: Date)(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(Since(TxDate(date))))
+
+
+  /** Get `List` of n rows as tuples matching molecule since date.
     * <br><br>
     * Get data added/retracted since a human point in time (a java.util.Date).
     * {{{
@@ -354,12 +595,6 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     *
     *   // Ben and Cay added since date1 - only n (1) rows returned
     *   Person.name.getSince(date1, 1) === List("Ben")
-    *
-    *   // Cay added since date2
-    *   Person.name.getSince(date2) === List("Cay")
-    *
-    *   // Nothing added since date3
-    *   Person.name.getSince(date3) === Nil
     * }}}
     *
     * @group getSince
@@ -369,7 +604,7 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     * @return List[Tpl] where Tpl is a tuple of data matching molecule
     * @see Equivalent asynchronous [[GetAsyncList.getAsyncSince(date:java\.util\.Date,n:Int)* getAsyncSince]] method.
     */
-  def getSince(date: Date, n: Int = -1)(implicit conn: Conn): List[Tpl] =
+  def getSince(date: Date, n: Int)(implicit conn: Conn): List[Tpl] =
     get(n)(conn.usingTempDb(Since(TxDate(date))))
 
 
@@ -447,7 +682,32 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     get(n)(conn.usingTempDb(With(txMolecules.flatten.flatten.map(_.toJava).asJava)))
 
 
-  /** Get `List` of rows as tuples matching molecule with applied raw transaction data.
+  /** Get `List` of all rows as tuples matching molecule with applied raw transaction data.
+    * <br><br>
+    * Apply raw transaction data to in-memory "branch" of db without affecting db to see how it would then look:
+    * {{{
+    *   // Live size of Person db
+    *   Person.name.get.size === 150
+    *
+    *   // Read some transaction data from file
+    *   val data_rdr2 = new FileReader("examples/resources/seattle/seattle-data1a.dtm")
+    *   val newDataTx = Util.readAll(data_rdr2).get(0).asInstanceOf[java.util.List[Object]]
+    *
+    *   // Imagine future db - 100 persons would be added, apparently
+    *   Person.name.getWith(newDataTx).size === 250
+    * }}}
+    *
+    * @group getWith
+    * @param txData Raw transaction data as java.util.List[Object]
+    * @param conn   Implicit [[molecule.datomic.base.facade.Conn Conn]] value in scope
+    * @return List[Tpl] where Tpl is a tuple of data matching molecule
+    * @see Equivalent asynchronous [[GetAsyncList.getAsyncWith(txData:java\.util\.List[_])* getAsyncWith]] method.
+    */
+  def getWith(txData: java.util.List[_])(implicit conn: Conn): List[Tpl] =
+    get(conn.usingTempDb(With(txData.asInstanceOf[jList[jList[_]]])))
+
+
+  /** Get `List` of n rows as tuples matching molecule with applied raw transaction data.
     * <br><br>
     * Apply raw transaction data to in-memory "branch" of db without affecting db to see how it would then look:
     * {{{
@@ -472,7 +732,7 @@ trait GetList[Obj, Tpl] extends GetArray[Obj, Tpl] with Quoted { self: Molecule_
     * @return List[Tpl] where Tpl is a tuple of data matching molecule
     * @see Equivalent asynchronous [[GetAsyncList.getAsyncWith(txData:java\.util\.List[_],n:Int)* getAsyncWith]] method.
     */
-  def getWith(txData: java.util.List[_], n: Int = -1)(implicit conn: Conn): List[Tpl] =
+  def getWith(txData: java.util.List[_], n: Int)(implicit conn: Conn): List[Tpl] =
     get(n)(conn.usingTempDb(With(txData.asInstanceOf[jList[jList[_]]])))
 
 
